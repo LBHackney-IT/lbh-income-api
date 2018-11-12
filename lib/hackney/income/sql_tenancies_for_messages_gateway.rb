d03c10a9c06ade5963affcaf3cbcb25e45508ef8
cob16 <public+github@cormacbrady.info>
2018-11-12 15:49:46 +0000

    extract tenancies_for_message_1 and pause querys into Tenancy scope

--- lib/hackney/income/sql_tenancies_for_messages_gateway.rb
+++ lib/hackney/income/sql_tenancies_for_messages_gateway.rb
@@ -2,12 +2,7 @@ module Hackney
   module Income
     class SqlTenanciesForMessagesGateway
       def get_tenancies_for_message_1
-        Hackney::Income::Models::Tenancy
-          .where(priority_band: 'green')
-          .where('days_in_arrears >= ?', 5)
-          .where('balance >= ?', 10.00)
-          .where(active_agreement: false)
-          .where('is_paused_until < ? OR is_paused_until is null', Date.today)
+        Hackney::Income::Models::Tenancy.tenancies_for_message_1
       end
     end
   end
