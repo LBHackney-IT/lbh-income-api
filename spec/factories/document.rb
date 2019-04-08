a2b5166b2524ae653645cb872c2637a4fc957110
Mac <mac@bbtnetworks.com>
2019-04-08 13:50:57 +0100

    should find the correct document states to poll against and enqueue them

--- spec/factories/document.rb
+++ spec/factories/document.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :document, class: Hackney::Cloud::Document do
     uuid { SecureRandom.uuid }
     ext_message_id { SecureRandom.uuid }
+    status { 'uploaded' }
   end
 end
