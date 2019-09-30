ce23678ef2e453aab6b12b144de7fc46c4491741
el <elena.vilimaite@gmail.com>
2019-09-30 10:48:07 +0100

    Revert "Nosp date saving (#208)"
    This reverts commit 36143da7c7140c3352fe823f76c66adcf4e78c4c.

--- db/migrate/20190927100815_add_nosp_served_date_to_case_priorities.rb
+++
@@ -1,5 +0,0 @@
-class AddNospServedDateToCasePriorities < ActiveRecord::Migration[5.2]
-  def change
-    add_column :case_priorities, :nosp_served_date, :datetime
-  end
-end
--- db/migrate/20190927102411_add_nosp_expiry_date_to_case_priorities.rb
+++
@@ -1,5 +0,0 @@
-class AddNospExpiryDateToCasePriorities < ActiveRecord::Migration[5.2]
-  def change
-    add_column :case_priorities, :nosp_expiry_date, :datetime
-  end
-end
