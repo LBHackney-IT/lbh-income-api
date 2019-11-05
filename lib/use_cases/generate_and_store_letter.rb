446cc59a891c394717126c1e62e642a4cb918257
George Schena <george@madetech.com>
2019-11-05 15:12:12 +0000

    Removed code which caches the letter

--- lib/use_cases/generate_and_store_letter.rb
+++ lib/use_cases/generate_and_store_letter.rb
@@ -4,6 +4,7 @@ module UseCases
   class GenerateAndStoreLetter
     def execute(payment_ref:, template_id:, user_id:)
       pdf_use_case_factory = Hackney::PDF::UseCaseFactory.new
+      letter_use_case_factory = Hackney::Letter::UseCaseFactory.new
 
       json = pdf_use_case_factory.get_preview.execute(
         payment_ref: payment_ref,
@@ -15,26 +16,21 @@ module UseCases
       uuid = json[:uuid]
       filename = "#{uuid}.pdf"
 
-      pop_letter_from_cache = UseCases::PopLetterFromCache.new(cache: Rails.cache)
-      letter = pop_letter_from_cache.execute(uuid: uuid)
-
       generate_pdf = UseCases::GeneratePdf.new
-      pdf = generate_pdf.execute(uuid: uuid, letter_html: letter[:preview])
+      pdf = generate_pdf.execute(uuid: uuid, letter_html: json[:preview])
 
-      create_document_model = UseCases::CreateDocumentModel.new(Hackney::Cloud::Document)
-      document_model = create_document_model.execute(
-        letter_html: letter[:preview],
+      document_model = letter_use_case_factory.create_document_model.execute(
+        letter_html: json[:preview],
         uuid: uuid,
         filename: filename,
         metadata: {
           user_id: user_id,
-          payment_ref: letter[:case][:payment_ref],
-          template: letter[:template]
+          payment_ref: json[:case][:payment_ref],
+          template: json[:template]
         }
       )
 
-      save_letter = UseCases::SaveLetterToCloud.new(Rails.configuration.cloud_adapter)
-      document_data = save_letter.execute(
+      document_data = letter_use_case_factory.save_letter_to_cloud.execute(
         filename: filename,
         bucket_name: Rails.application.config_for('cloud_storage')['bucket_docs'],
         pdf: pdf
