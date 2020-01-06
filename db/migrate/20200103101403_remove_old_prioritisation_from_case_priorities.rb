18745320f72b191f0693eb80420ee1dff21047cb
el <elena@madetech.com>
2020-01-06 09:13:58 +0000

    typo fix

--- db/migrate/20200103101403_remove_old_prioritisation_from_case_priorities.rb
+++ db/migrate/20200103101403_remove_old_prioritisation_from_case_priorities.rb
@@ -9,7 +9,7 @@ class RemoveOldPrioritisationFromCasePriorities < ActiveRecord::Migration[5.2]
     remove_column :case_priorities, :days_since_last_payment_contribution, :decimal
     remove_column :case_priorities, :payment_amount_delta_contribution, :decimal
     remove_column :case_priorities, :payment_date_delta_contribution, :decimal
-    remove_column :case_priorities, :umber_of_broken_agreements_contribution, :decimal
+    remove_column :case_priorities, :number_of_broken_agreements_contribution, :decimal
     remove_column :case_priorities, :active_agreement_contribution, :decimal
     remove_column :case_priorities, :broken_court_order_contribution, :decimal
     remove_column :case_priorities, :osp_served_contribution, :decimal
