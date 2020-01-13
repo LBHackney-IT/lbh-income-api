d371a8cf14463570887ecb63f27238b903426216
George Schena <george@madetech.com>
2020-01-13 11:59:17 +0000

    Refactor tests

--- spec/lib/hackney/income/tenancy_prioritiser/classifications/court_breach_visit_spec.rb
+++ spec/lib/hackney/income/tenancy_prioritiser/classifications/court_breach_visit_spec.rb
@@ -4,88 +4,38 @@ describe 'Send Court Breach Letter Rule', type: :feature do
   court_breach_letter_code = Hackney::Tenancy::ActionCodes::COURT_BREACH_LETTER_SENT
   court_warning_letter_code = Hackney::Tenancy::ActionCodes::COURT_WARNING_LETTER_SENT
 
-  send_court_breach_letter_condition_matrix = [
-    {
+  base_example = {
+    outcome: :court_breach_visit,
+    nosps_in_last_year: 0,
+    weekly_rent: 5,
+    is_paused_until: nil,
+    balance: 15.0, # 3 * weekly_rent
+    active_agreement: false,
+    last_communication_action: court_breach_letter_code,
+    last_communication_date: Date.today + 21
+  }
+
+  examples = [
+    base_example,
+    base_example.merge(
+      description: 'with a valid last_communication_action',
       outcome: :court_breach_visit,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
       last_communication_action: court_breach_letter_code,
       last_communication_date: Date.today + 21
-    },
-    {
+    ),
+    base_example.merge(
+      description: 'with a date outside of 3 months',
       outcome: :send_letter_one,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
       last_communication_action: court_breach_letter_code,
-      last_communication_date: 20.weeks.ago.to_date
-    },
-    {
-      outcome: :court_breach_visit,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
-      last_communication_action: court_breach_letter_code,
-      last_communication_date: 3.months.ago
-    },
-    {
+      last_communication_date: Date.today - 420
+    ),
+    base_example.merge(
+      description: 'with a last_communication_action which is NOT a court_breach_letter_code',
       outcome: :no_action,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
       last_communication_action: court_warning_letter_code,
       last_communication_date: Date.today + 21
-    },
-    {
-      outcome: :court_breach_visit,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
-      last_communication_action: court_breach_letter_code,
-      last_communication_date: Date.today + 21
-    },
-    {
-      outcome: :send_letter_one,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
-      last_communication_action: court_breach_letter_code,
-      last_communication_date: Date.today - 200
-    },
-    {
-      outcome: :no_action,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
-      last_communication_action: court_warning_letter_code,
-      last_communication_date: Date.today + 21
-    },
-    {
-      outcome: :send_letter_one,
-      nosps_in_last_year: 0,
-      weekly_rent: 5,
-      is_paused_until: nil,
-      balance: 15.0, # 3 * weekly_rent
-      active_agreement: false,
-      last_communication_action: '',
-      last_communication_date: Date.today + 21
-    }
+    )
   ]
 
-  it_behaves_like 'TenancyClassification', send_court_breach_letter_condition_matrix
+  include_examples 'TenancyClassification', examples
 end
