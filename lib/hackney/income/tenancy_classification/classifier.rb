8e7eddfbf9ae039ebc91566ba4a6762ccbea74c5
Antony O'Neill <antony.oneill@madetech.com>
2020-06-01 15:24:08 +0100

    Rename classifier

--- lib/hackney/income/tenancy_classification/classifier.rb
+++ lib/hackney/income/tenancy_classification/classifier.rb
@@ -3,12 +3,12 @@ module Hackney
     module TenancyClassification
       class Classifier
         def initialize(case_priority, criteria, documents)
-          @version1 = Hackney::Income::TenancyClassification::V1::Classifier.new(
+          @version1_classifier = Hackney::Income::TenancyClassification::V1::Classifier.new(
             case_priority,
             criteria,
             documents
           )
-          @version2 = Hackney::Income::TenancyClassification::V2::Classifier.new(
+          @version2_classifier = Hackney::Income::TenancyClassification::V2::Classifier.new(
             case_priority,
             criteria,
             documents
@@ -16,8 +16,8 @@ module Hackney
         end
 
         def execute
-          version1_action = @version1.execute
-          version2_action = @version2.execute
+          version1_action = @version1_classifier.execute
+          version2_action = @version2_classifier.execute
 
           Rails.logger.error("The action from V1 #{version1_action} does not match V2 #{version2_action}") if version1_action != version2_action
 
