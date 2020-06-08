8e4d7f04ae2ca5ad4b3ec7458a2ae1e78de1030e
Ninamma <ninamma.rai@outlook.com>
2020-06-08 17:22:37 +0100

    move out UpdateCourtOutcomeAction logic from classifier

---
+++ lib/hackney/income/tenancy_classification/v2/rulesets/update_court_outcome_action.rb
@@ -0,0 +1,25 @@
+module Hackney
+  module Income
+    module TenancyClassification
+      module V2
+        module Rulesets
+          class UpdateCourtOutcomeAction < BaseRuleset
+            def execute
+              return :update_court_outcome_action if action_valid
+            end
+
+            private
+
+            def action_valid
+              return false if should_prevent_action?
+              return false if @criteria.courtdate.blank?
+              return false if @criteria.courtdate.future?
+
+              @criteria.court_outcome.blank?
+            end
+          end
+        end
+      end
+    end
+  end
+end
