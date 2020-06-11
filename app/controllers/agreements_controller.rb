9d3dab09e4c0ef083e4a25d33fe26f352ff6eb91
Ninamma <ninamma.rai@outlook.com>
2020-06-11 15:07:44 +0100

    Move view agreements Use case into income use case factory for continuity

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -2,7 +2,7 @@ class AgreementsController < ApplicationController
   def index
     tenancy_ref = agreements_params.fetch(:tenancy_ref)
 
-    response = Hackney::Income::ViewAgreements.execute(tenancy_ref: tenancy_ref)
+    response = income_use_case_factory.view_agreements.execute(tenancy_ref: tenancy_ref)
 
     render json: response
   end
