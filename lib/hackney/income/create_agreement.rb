bbbce7ec8608783a3873c67dacb6276fb76e16cc
Ninamma <ninamma.rai@outlook.com>
2020-06-17 11:30:04 +0100

    Allow CreateAgreement usecase to cancel previous live agreement
    Also used AgreementHelper instead of calling ViewAgreements usecase
    Co-authored-by: Csaba Gyorfi <csaba@madetech.com>

--- lib/hackney/income/create_agreement.rb
+++ lib/hackney/income/create_agreement.rb
@@ -1,10 +1,10 @@
 module Hackney
   module Income
     class CreateAgreement
-      def self.execute(new_agreement_params:)
+      include AgreementHelper
+
+      def execute(new_agreement_params:)
         case_details = Hackney::Income::Models::CasePriority.where(tenancy_ref: new_agreement_params[:tenancy_ref])
-        # case_details is as array, but I think each tag_ref has one case priority?
-        # In the Case model it say: 'has_one :case_priority'
 
         agreement_params = {
           tenancy_ref: new_agreement_params[:tenancy_ref],
@@ -13,15 +13,21 @@ module Hackney
           amount: new_agreement_params[:amount],
           start_date: new_agreement_params[:start_date],
           frequency: new_agreement_params[:frequency],
-          current_state: 'active' # Need some magic to determine state of agreement - here? or elsewhere?
-          # current_state: new_agreement_params[:current_state] || 'active'
+          current_state: 'live'
         }
 
-        Hackney::Income::Models::Agreement.create!(agreement_params)
+        live_agreements = Hackney::Income::Models::Agreement.where(tenancy_ref: new_agreement_params[:tenancy_ref]).where(current_state: 'live')
+
+        if live_agreements.any?
+          live_agreements.each do |agreement|
+            Hackney::Income::Models::AgreementState.create!(agreement_id: agreement.id, agreement_state: 'cancelled')
+          end
+        end
+
+        new_agreement = Hackney::Income::Models::Agreement.create!(agreement_params)
         Hackney::Income::Models::AgreementState.create!(agreement_id: new_agreement.id, agreement_state: agreement_params[:current_state])
 
-        # Is calling another use case bad? Should I just build the response using new_agreement here again instead?
-        Hackney::Income::ViewAgreements.execute(tenancy_ref: new_agreement_params[:tenancy_ref])[:agreements].last
+        create_agreement_response(agreement: new_agreement)
       end
     end
   end
--- spec/lib/hackney/income/create_agreement_spec.rb
+++ spec/lib/hackney/income/create_agreement_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Hackney::Income::CreateAgreement do
-  subject { described_class.execute(new_agreement_params: new_agreement_params) }
+  subject { described_class.new }
 
   let(:tenancy_ref) { Faker::Number.number(digits: 2).to_s }
   let(:agreement_type) { 'informal' }
@@ -9,77 +9,58 @@ describe Hackney::Income::CreateAgreement do
   let(:start_date) { Faker::Date.between(from: 2.days.ago, to: Date.today) }
   let(:frequency) { 'weekly' }
 
+  let(:existing_agreement_params) do
+    {
+      tenancy_ref: tenancy_ref,
+      agreement_type: 'formal',
+      amount: Faker::Commerce.price(range: 10...100),
+      start_date: Faker::Date.between(from: 4.days.ago, to: Date.today),
+      frequency: frequency
+    }
+  end
 
-  # let(:existing_agreement_params) do
-  #   {
-  #     tenancy_ref: tenancy_ref.to_s,
-  #     agreement_type: 'formal',
-  #     amount: 5.to_s,
-  #     start_date: Faker::Date.between(from: 4.days.ago, to: Date.today).to_s,
-  #     frequency: frequency,
-  #     current_state: 'breached'
-  #   }
-  # end
-
-
-  # let(:existing_agreement_params) do
-  #   {
-  #     tenancy_ref: tenancy_ref.to_s,
-  #     agreement_type: 'formal',
-  #     amount: 5.to_s,
-  #     start_date: Faker::Date.between(from: 4.days.ago, to: Date.today).to_s,
-  #     frequency: frequency,
-  #     current_state: 'breached'
-  #   }
-  # end
-
-  # Would the parameters be passed to the use case as strings??
   let(:new_agreement_params) do
     {
-      tenancy_ref: tenancy_ref.to_s,
+      tenancy_ref: tenancy_ref,
       agreement_type: agreement_type,
-      amount: amount.to_s,
-      start_date: start_date.to_s,
+      amount: amount,
+      start_date: start_date,
       frequency: frequency
     }
   end
 
   context 'when there are no previous agreements for the tenancy' do
-    it 'creates and returns a new active agreement with the new agreement in its history' do
+    it 'creates and returns a new live agreement' do
       Hackney::Income::Models::CasePriority.create!(tenancy_ref: tenancy_ref, balance: 100)
 
-      expect(subject[:tenancyRef]).to eq(tenancy_ref)
-      expect(subject[:agreementType]).to eq(agreement_type)
-      expect(subject[:amount]).to eq(amount)
-      expect(subject[:startDate]).to eq(start_date)
-      expect(subject[:frequency]).to eq(frequency)
-      expect(subject[:currentState]).to eq('active')
-      expect(subject[:history].count).to eq(1)
-      expect(subject[:history].first[:state]).to eq('active')
-      expect(subject[:startingBalance]).to eq(100)
+      response = subject.execute(new_agreement_params: new_agreement_params)
+
+      expect(response[:tenancyRef]).to eq(tenancy_ref)
+      expect(response[:agreementType]).to eq(agreement_type)
+      expect(response[:amount]).to eq(amount)
+      expect(response[:startDate]).to eq(start_date)
+      expect(response[:frequency]).to eq(frequency)
+      expect(response[:currentState]).to eq('live')
+      expect(response[:history].count).to eq(1)
+      expect(response[:history].first[:state]).to eq('live')
+      expect(response[:startingBalance]).to eq(100)
     end
   end
-  # I spent way too long being confused because I thought the history was meant to show the  agreements
-  # for a tenancy ref - the I realised, it's the different states one agreement has been in
-  # Although are we doing new agreements for one tenancy_ref as different agreements?
-  #
-  # context 'when there is a previous agreement for the tenancy' do
-  #   it 'creates and returns a new active agreement with the two agreements in history' do
-  #     Hackney::Income::Models::CasePriority.create!(tenancy_ref: tenancy_ref, balance: 200)
-  #
-  #     existing_agreement = described_class.execute(new_agreement_params: existing_agreement_params)
-  #     existing_agreement2 = described_class.execute(new_agreement_params: existing_agreement2_params)
-  #
-  #     expect(subject[:tenancyRef]).to eq(tenancy_ref)
-  #     expect(subject[:agreementType]).to eq(agreement_type)
-  #     expect(subject[:amount]).to eq(amount)
-  #     expect(subject[:startDate]).to eq(start_date)
-  #     expect(subject[:frequency]).to eq(frequency)
-  #     expect(subject[:currentState]).to eq('active')
-  #
-  #     expect(subject[:history].count).to eq(2)
-  #     expect(subject[:history].first[:state]).to eq('breached')
-  #     expect(subject[:startingBalance]).to eq(200)
-  #   end
-  # end
+
+  context 'when there is a previous agreement for the tenancy' do
+    it "creates and returns a new live agreement and the previous agreement's state is set to 'cancelled' " do
+      Hackney::Income::Models::CasePriority.create!(tenancy_ref: tenancy_ref, balance: 200)
+
+      existing_agreement = subject.execute(new_agreement_params: existing_agreement_params)
+      new_agreement = subject.execute(new_agreement_params: new_agreement_params)
+
+      agreements = Hackney::Income::Models::Agreement.where(tenancy_ref: tenancy_ref).includes(:agreement_states)
+
+      expect(agreements.count).to eq(2)
+
+      expect(agreements.first.tenancy_ref).to eq(existing_agreement[:tenancyRef])
+      expect(agreements.second.tenancy_ref).to eq(new_agreement[:tenancyRef])
+      expect(agreements.first.current_state).to eq('cancelled')
+    end
+  end
 end
