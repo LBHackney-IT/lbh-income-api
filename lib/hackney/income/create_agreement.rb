d33d911b31aa826f8d8ec02c1c972c4e0f38f2a5
csabagyorfi <csaba@madetech.com>
2020-07-21 13:18:42 +0100

    Add create agreement parent class

---
+++ lib/hackney/income/create_agreement.rb
@@ -0,0 +1,40 @@
+module Hackney
+  module Income
+    class CreateAgreement
+      def initialize(add_action_diary:, cancel_agreement:)
+        @add_action_diary = add_action_diary
+        @cancel_agreement = cancel_agreement
+      end
+
+      def find_case_details(tenancy_ref)
+        Hackney::Income::Models::CasePriority.where(tenancy_ref: tenancy_ref).first
+      end
+
+      def create_agreement(agreement_params)
+        new_agreement = Hackney::Income::Models::Agreement.create!(agreement_params)
+        Hackney::Income::Models::AgreementState.create!(agreement_id: new_agreement.id, agreement_state: :live)
+
+        new_agreement
+      end
+
+      def add_action_diary_entry(tenancy_ref:, comment:, created_by:)
+        @add_action_diary.execute(
+          tenancy_ref: tenancy_ref,
+          action_code: 'AGR',
+          comment: comment,
+          username: created_by
+        )
+      end
+
+      def cancel_active_agreements(tenancy_ref)
+        active_agreements = Hackney::Income::Models::Agreement.where(tenancy_ref: tenancy_ref).select(&:active?)
+
+        return unless active_agreements.any?
+
+        active_agreements.each do |agreement|
+          @cancel_agreement.execute(agreement_id: agreement.id)
+        end
+      end
+    end
+  end
+end
