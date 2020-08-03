7d531101224b88b08ec8766c4b72f2e058cfa434
Elena <elena@madetech.com>
2020-08-03 15:39:49 +0100

    add filtering

--- lib/hackney/income/fetch_actions_gateway.rb
+++ lib/hackney/income/fetch_actions_gateway.rb
@@ -4,17 +4,15 @@ module Hackney
       GatewayModel = Hackney::IncomeCollection::Action
 
       def number_of_pages(number_per_page:, filters: {}, service_area_type:)
-        (GatewayModel.where(service_area_type: service_area_type).count.to_f / number_per_page).ceil
-        # byebug
+        (actions_filtered_for(service_area_type, filters).count.to_f / number_per_page).ceil
       end
 
       def get_actions(page_number: nil, number_per_page: nil, filters: {}, service_area_type:)
-        query = GatewayModel.where(service_area_type: service_area_type)
+        query = actions_filtered_for(service_area_type, filters)
 
         query = query.offset((page_number - 1) * number_per_page).limit(number_per_page) if page_number.present? && number_per_page.present?
-        # order_options   = 'eviction_date' if filters[:upcoming_evictions].present?
-        # order_options   = 'courtdate' if filters[:upcoming_court_dates].present?
-        # order_options   = 'is_paused_until' if filters[:is_paused]
+
+        order_options   = 'is_paused_until' if filters[:is_paused]
         order_options ||= by_balance
 
         query.order(order_options)
@@ -22,8 +20,8 @@ module Hackney
 
       private
 
-      def tenancies_filtered_for(filters)
-        query = GatewayModel.where('balance > ?', 0)
+      def actions_filtered_for(service_area_type, filters)
+        query = GatewayModel.where(service_area_type: service_area_type).where('balance > ?', 0)
 
         if filters[:patch].present?
           if filters[:patch] == 'unassigned'
@@ -32,9 +30,6 @@ module Hackney
             query = query.where(patch_code: filters[:patch])
           end
         end
-
-        query = query.where('eviction_date >= ?', Time.zone.now.beginning_of_day) if filters[:upcoming_evictions].present?
-        query = query.where('courtdate >= ?', Time.zone.now.beginning_of_day) if filters[:upcoming_court_dates].present?
 
         if filters[:classification].present?
           query = query.where(classification: filters[:classification])
@@ -61,31 +56,6 @@ module Hackney
 
       def by_balance
         Arel.sql('balance DESC')
-      end
-
-      def build_tenancy_list_item(model)
-        {
-          tenancy_ref: model.tenancy_ref,
-          balance: model.balance,
-          days_in_arrears: model.days_in_arrears,
-          days_since_last_payment: model.days_since_last_payment,
-          number_of_broken_agreements: model.number_of_broken_agreements,
-          active_agreement: model.active_agreement,
-          broken_court_order: model.broken_court_order,
-          nosp_served: model.nosp_served,
-          active_nosp: model.active_nosp,
-          patch_code: model.patch_code,
-          classification: model.classification,
-          courtdate: model.courtdate,
-          court_outcome: model.court_outcome,
-          eviction_date: model.eviction_date,
-          latest_active_agreement_date: model.latest_active_agreement_date,
-          breach_agreement_date: model.breach_agreement_date,
-          expected_balance: model.expected_balance,
-          pause_reason: model.pause_reason,
-          pause_comment: model.pause_comment,
-          is_paused_until: model.is_paused_until
-        }
       end
     end
   end
