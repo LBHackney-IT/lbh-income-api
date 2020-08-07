f0a0dc9773c618c9b666f73df30319bb44f56844
csabagyorfi <csaba@madetech.com>
2020-08-07 15:31:44 +0100

    Refactor update agreement state use-case
    Remove the responsibility of looking up the current balance by adding
    the current_balance as an arg when executing the usecase

--- lib/hackney/income/update_all_agreement_state.rb
+++ lib/hackney/income/update_all_agreement_state.rb
@@ -1,6 +1,8 @@
 module Hackney
   module Income
     class UpdateAllAgreementState
+      WorktrayItem = Hackney::Income::Models::CasePriority
+
       def initialize(update_agreement_state:)
         @update_agreement_state = update_agreement_state
       end
@@ -10,7 +12,8 @@ module Hackney
           Hackney::Income::Models::Agreement.where('current_state =? OR current_state =?', 'live', 'breached')
 
         all_active_agreements.each do |agreement|
-          @update_agreement_state.execute(agreement: agreement)
+          current_balance = WorktrayItem.find_by(tenancy_ref: agreement.tenancy_ref).balance
+          @update_agreement_state.execute(agreement: agreement, current_balance: current_balance)
         rescue StandardError => e
           puts "[#{Time.now}] Failed to update agreement state " \
                "for agreement: #{agreement.id} of tenancy: #{agreement.tenancy_ref}, with error: #{e.inspect}."
