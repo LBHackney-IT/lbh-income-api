72fd15466d6df404190b0b548d0150139c7e64c7
Ninamma <ninamma.rai@outlook.com>
2020-08-09 16:44:58 +0100

    Add terms and disrepair column for adjourned court cases
    Updated model based on this too

--- app/models/hackney/income/models/court_case.rb
+++ app/models/hackney/income/models/court_case.rb
@@ -3,7 +3,13 @@ module Hackney
     module Models
       class CourtCase < ApplicationRecord
         validates_presence_of :tenancy_ref
+        validates_presence_of :terms, if: :adjourned?
+        validates_presence_of :disrepair_counter_claim, if: :adjourned?
         has_many :agreements, -> { where agreement_type: :formal }, class_name: 'Hackney::Income::Models::Agreement'
+        
+        def adjourned?
+          court_outcome&.start_with?('A')
+        end
       end
     end
   end
