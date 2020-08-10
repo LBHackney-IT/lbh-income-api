2dff6f42929a8c5b79a824ffea17e5499f1b39f5
csabagyorfi <csaba@madetech.com>
2020-08-10 18:14:53 +0100

    Create MAAgreementHelpers class
    This should be implemented by TenancyClassification::V2::Helpers once we
    start using the new agreement system in MA

---
+++ lib/hackney/income/tenancy_classification/v2/helpers/helpers_base.rb
@@ -0,0 +1,13 @@
+module Hackney
+  module Income
+    module TenancyClassification
+      module V2
+        module Helpers
+          module HelpersBase
+            def should_prevent_action?; end
+          end
+        end
+      end
+    end
+  end
+end
