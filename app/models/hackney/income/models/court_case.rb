b6589cf9ec011e5efe22975c534c686a4b7af125
csabagyorfi <csaba@madetech.com>
2020-08-13 12:32:19 +0100

    Add tighter check for adjourned? on court case model
    It can fail test when faker generates court_outcome that starts with 'a'

--- app/models/hackney/income/models/court_case.rb
+++ app/models/hackney/income/models/court_case.rb
@@ -8,7 +8,12 @@ module Hackney
         has_many :agreements, -> { where agreement_type: :formal }, class_name: 'Hackney::Income::Models::Agreement'
 
         def adjourned?
-          court_outcome&.start_with?('A')
+          [
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_GENERALLY_WITH_PERMISSION_TO_RESTORE,
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_TO_NEXT_OPEN_DATE,
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_TO_ANOTHER_HEARING_DATE,
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING
+          ].include?(court_outcome)
         end
       end
     end
