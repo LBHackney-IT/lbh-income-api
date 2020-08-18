8d5d3005d7d99545055df0275e410e6071eb2dd8
Antony O'Neill <antony.oneill@madetech.com>
2020-08-18 17:52:22 +0100

    Migrate court cases which contain a court outcome
    Even if they don't have a court date.

--- lib/hackney/income/migrate_uh_court_case.rb
+++ lib/hackney/income/migrate_uh_court_case.rb
@@ -6,14 +6,19 @@ module Hackney
       end
 
       def migrate(criteria)
-        @create_court_case.execute(map_criteria_to_court_case_params(criteria)) if criteria.courtdate
+        @create_court_case.execute(map_criteria_to_court_case_params(criteria)) if should_migrate_criteria(criteria)
       end
 
       private
 
+      def should_migrate_criteria(criteria)
+        criteria.courtdate.present? || criteria.court_outcome.present?
+      end
+
       def map_criteria_to_court_case_params(criteria)
         {
-          court_date: criteria.courtdate
+          court_date: criteria.courtdate,
+          court_outcome: criteria.court_outcome
         }
       end
     end
--- spec/lib/hackney/income/migrate_uh_court_case_spec.rb
+++ spec/lib/hackney/income/migrate_uh_court_case_spec.rb
@@ -31,7 +31,46 @@ describe Hackney::Income::MigrateUhCourtCase do
 
     it 'creates a partial court case' do
       expect(create_court_case).to receive(:execute).with(
-        court_date: criteria_attributes[:courtdate]
+        hash_including(
+          court_date: criteria_attributes[:courtdate]
+        )
+      )
+      subject
+    end
+  end
+
+  context 'when provided a criteria with a court outcome but without a court date' do
+    let(:criteria_attributes) {
+      {
+        court_outcome: Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING,
+        courtdate: nil
+      }
+    }
+
+    it 'creates a partial court case' do
+      expect(create_court_case).to receive(:execute).with(
+        hash_including(
+          court_outcome: criteria_attributes[:court_outcome]
+        )
+      )
+      subject
+    end
+  end
+
+  context 'when provided a criteria with a court date and a court outcome' do
+    let(:criteria_attributes) {
+      {
+        court_outcome: Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING,
+        courtdate: DateTime.now.midnight - 7.days
+      }
+    }
+
+    it 'creates a partial court case' do
+      expect(create_court_case).to receive(:execute).with(
+        hash_including(
+          court_date: criteria_attributes[:courtdate],
+          court_outcome: criteria_attributes[:court_outcome]
+        )
       )
       subject
     end
