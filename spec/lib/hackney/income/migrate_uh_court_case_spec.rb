62a779824eba7fb206d533b3a1c43d147f81f158
Antony O'Neill <antony.oneill@madetech.com>
2020-08-19 08:48:41 +0100

    Organise no court case tests

--- spec/lib/hackney/income/migrate_uh_court_case_spec.rb
+++ spec/lib/hackney/income/migrate_uh_court_case_spec.rb
@@ -7,72 +7,74 @@ describe Hackney::Income::MigrateUhCourtCase do
 
   let(:criteria) { Stubs::StubCriteria.new(criteria_attributes) }
 
-  context 'when provided a criteria without a court date or court outcome' do
-    let(:criteria_attributes) {
-      {
-        court_outcome: nil,
-        courtdate: nil
+  context 'when there is no existing court case' do
+    context 'when provided a criteria without a court date or court outcome' do
+      let(:criteria_attributes) {
+        {
+          court_outcome: nil,
+          courtdate: nil
+        }
       }
-    }
 
-    it 'does not create a court case' do
-      expect(create_court_case).not_to receive(:execute)
-      subject
+      it 'does not create a court case' do
+        expect(create_court_case).not_to receive(:execute)
+        subject
+      end
     end
-  end
 
-  context 'when provided a criteria with a court date but without a court outcome' do
-    let(:criteria_attributes) {
-      {
-        court_outcome: nil,
-        courtdate: DateTime.now.midnight - 7.days
+    context 'when provided a criteria with a court date but without a court outcome' do
+      let(:criteria_attributes) {
+        {
+          court_outcome: nil,
+          courtdate: DateTime.now.midnight - 7.days
+        }
       }
-    }
 
-    it 'creates a partial court case' do
-      expect(create_court_case).to receive(:execute).with(
-        hash_including(
-          court_date: criteria_attributes[:courtdate]
+      it 'creates a partial court case' do
+        expect(create_court_case).to receive(:execute).with(
+          hash_including(
+            court_date: criteria_attributes[:courtdate]
+          )
         )
-      )
-      subject
+        subject
+      end
     end
-  end
 
-  context 'when provided a criteria with a court outcome but without a court date' do
-    let(:criteria_attributes) {
-      {
-        court_outcome: Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING,
-        courtdate: nil
+    context 'when provided a criteria with a court outcome but without a court date' do
+      let(:criteria_attributes) {
+        {
+          court_outcome: Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING,
+          courtdate: nil
+        }
       }
-    }
 
-    it 'creates a partial court case' do
-      expect(create_court_case).to receive(:execute).with(
-        hash_including(
-          court_outcome: criteria_attributes[:court_outcome]
+      it 'creates a partial court case' do
+        expect(create_court_case).to receive(:execute).with(
+          hash_including(
+            court_outcome: criteria_attributes[:court_outcome]
+          )
         )
-      )
-      subject
+        subject
+      end
     end
-  end
 
-  context 'when provided a criteria with a court date and a court outcome' do
-    let(:criteria_attributes) {
-      {
-        court_outcome: Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING,
-        courtdate: DateTime.now.midnight - 7.days
+    context 'when provided a criteria with a court date and a court outcome' do
+      let(:criteria_attributes) {
+        {
+          court_outcome: Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING,
+          courtdate: DateTime.now.midnight - 7.days
+        }
       }
-    }
 
-    it 'creates a partial court case' do
-      expect(create_court_case).to receive(:execute).with(
-        hash_including(
-          court_date: criteria_attributes[:courtdate],
-          court_outcome: criteria_attributes[:court_outcome]
+      it 'creates a partial court case' do
+        expect(create_court_case).to receive(:execute).with(
+          hash_including(
+            court_date: criteria_attributes[:courtdate],
+            court_outcome: criteria_attributes[:court_outcome]
+          )
         )
-      )
-      subject
+        subject
+      end
     end
   end
 end
