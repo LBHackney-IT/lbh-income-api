a664db756c05a2d02ddf44fcdb7fa65fcff4eaab
csabagyorfi <csaba@madetech.com>
2020-08-24 11:10:24 +0100

    Add validation for court outcome on court case model

--- app/models/hackney/income/models/court_case.rb
+++ app/models/hackney/income/models/court_case.rb
@@ -5,15 +5,34 @@ module Hackney
         validates_presence_of :tenancy_ref
         validates_presence_of :terms, if: :adjourned?
         validates_presence_of :disrepair_counter_claim, if: :adjourned?
+        validate :court_outcome_is_valid
         has_many :agreements, -> { where agreement_type: :formal }, class_name: 'Hackney::Income::Models::Agreement'
 
-        def adjourned?
+        ADJOURNED_COURT_OUTCOMES =
           [
             Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_GENERALLY_WITH_PERMISSION_TO_RESTORE,
             Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_TO_NEXT_OPEN_DATE,
             Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_TO_ANOTHER_HEARING_DATE,
             Hackney::Tenancy::UpdatedCourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING
-          ].include?(court_outcome)
+          ].freeze
+
+        OTHER_COURT_OUTCOMES =
+          [
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::SUSPENSION_ON_TERMS,
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::STRUCK_OUT,
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::WITHDRAWN_ON_THE_DAY,
+            Hackney::Tenancy::UpdatedCourtOutcomeCodes::STAY_OF_EXECUTION
+          ].freeze
+
+        def adjourned?
+          ADJOURNED_COURT_OUTCOMES.include?(court_outcome)
+        end
+
+        private
+
+        def court_outcome_is_valid
+          return unless court_outcome.present?
+          errors.add(:court_outcome, 'must be a valid court outcome code') unless (ADJOURNED_COURT_OUTCOMES + OTHER_COURT_OUTCOMES).include?(court_outcome)
         end
       end
     end
