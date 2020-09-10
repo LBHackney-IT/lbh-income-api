44c1de77c65931433e051c12b3c6b2bcd3e4c999
Miles-Alford <miles.alford@hackney.gov.uk>
2020-09-10 16:16:17 +0100

    Update court outcome letter to use correct data

--- app/models/hackney/income_collection/letter/court_outcome.rb
+++ app/models/hackney/income_collection/letter/court_outcome.rb
@@ -9,22 +9,31 @@ module Hackney
         ].freeze
         MANDATORY_FIELDS = %i[court_outcome].freeze
 
-        attr_reader :court_outcome, :court_hearing_arrears, :instalment_amount, :frequency, :rent, :weekly_payment, :date_of_first_payment
+        attr_reader :court_outcome, :court_hearing_arrears, :instalment_amount, :frequency, :rent, :date_of_first_payment, :eviction_date, :rent_charge, :total_amount_payable
 
         def initialize(params)
           super(params)
+          if formal_agreement?
+            mandatory_outcome_fields = MANDATORY_FIELDS + %i[court_hearing_arrears instalment_amount frequency rent date_of_first_payment rent_charge total_amount_payable]
+          elsif outright_order?
+            mandatory_outcome_fields = MANDATORY_FIELDS + %i[eviction_date]
+          elsif formal_agreement? && outright_order?
+            mandatory_outcome_fields = MANDATORY_FIELDS + %i[court_hearing_arrears instalment_amount frequency rent date_of_first_payment eviction_date rent_charge total_amount_payable]
+          else
+            mandatory_outcome_fields = MANDATORY_FIELDS
+          end
 
-          validated_params = validate_mandatory_fields(MANDATORY_FIELDS, params)
+          validated_params = validate_mandatory_fields(mandatory_outcome_fields, params)
           @court_outcome = validated_params[:court_outcome]
-
 
           @court_hearing_arrears = validated_params[:court_hearing_arrears]
           @instalment_amount = format('%.2f', validated_params[:amount]) unless validated_params[:amount].nil?
-          @frequency = validated_params[:frequency]
+          @agreement_frequency = validated_params[:frequency]
           @rent = validated_params[:rent]
-          @weekly_payment = validated_params[:weekly_payment]
           @date_of_first_payment = format_date(validated_params[:date_of_first_payment])
-          validate_informal_agreement_fields_exist
+          @eviction_date = format_date(validated_params[:eviction_date])
+          @rent_charge = format('%.2f', calculate_rent(@rent, @agreement_frequency))
+          @total_amount_payable = format('%.2f', calculate_total_amount_payable(@rent_charge, @instalment_amount))
         end
 
         private
@@ -35,17 +44,6 @@ module Hackney
 
         def outright_order?
         @court_outcome == Hackney::Tenancy::UpdatedCourtOutcomeCodes::OUTRIGHT_POSSESSION_FORTHWITH || Hackney::Tenancy::UpdatedCourtOutcomeCodes::OUTRIGHT_POSSESSION_WITH_DATE
-        end
-
-        def validate_informal_agreement_fields_exist
-          if formal_agreement?
-          @errors.concat(name: @court_hearing_arrears.to_s, message: 'missing mandatory field') if @court_hearing_arrears.nil?
-          @errors.concat(name: @instalment_amount.to_s, message: 'missing mandatory field') if @instalment_amount.nil?
-          @errors.concat(name: @frequency.to_s, message: 'missing mandatory field') if @frequency.nil?
-          @errors.concat(name: @rent.to_s, message: 'missing mandatory field') if @rent.nil?
-          @errors.concat(name: @weekly_payment.to_s, message: 'missing mandatory field') if @weekly_payment.nil?
-          @errors.concat(name: @date_of_first_payment.to_s, message: 'missing mandatory field') if @date_of_first_payment.nil?
-          end
         end
       end
     end
