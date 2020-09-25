95be3517c55860fcb4f9e54734b57c7c2c2c6545
Ninamma Rai <Ninamma.rai@outlook.com>
2020-09-25 12:07:30 +0100

    Use static variables for outcome codes, amend Stay of execution
    As requested

--- app/models/hackney/court_outcome_helper.rb
+++ app/models/hackney/court_outcome_helper.rb
@@ -2,17 +2,17 @@ module Hackney
   module CourtOutcomeHelper
     def human_readable_outcome(code)
       code_mapping = {
-        'AGP' => 'Adjourned generally with permission to restore',
-        'AND' => 'Adjourned to next open date',
-        'AAH' => 'Adjourned to another hearing date',
-        'ADH' => 'Adjourned for directions hearing',
-        'ADT' => 'Adjourned on terms',
-        'OPF' => 'Outright possession forthwith',
-        'OPD' => 'Outright possession with date',
-        'SOT' => 'Suspension on terms',
-        'STO' => 'Struck out',
-        'WIT' => 'Withdrawn on the day',
-        'SOE' => 'Stay of execution'
+        Hackney::Tenancy::CourtOutcomeCodes::ADJOURNED_GENERALLY_WITH_PERMISSION_TO_RESTORE => 'Adjourned generally with permission to restore',
+        Hackney::Tenancy::CourtOutcomeCodes::ADJOURNED_TO_NEXT_OPEN_DATE => 'Adjourned to next open date',
+        Hackney::Tenancy::CourtOutcomeCodes::ADJOURNED_TO_ANOTHER_HEARING_DATE => 'Adjourned to another hearing date',
+        Hackney::Tenancy::CourtOutcomeCodes::ADJOURNED_FOR_DIRECTIONS_HEARING => 'Adjourned for directions hearing',
+        Hackney::Tenancy::CourtOutcomeCodes::ADJOURNED_ON_TERMS => 'Adjourned on terms',
+        Hackney::Tenancy::CourtOutcomeCodes::OUTRIGHT_POSSESSION_FORTHWITH => 'Outright possession forthwith',
+        Hackney::Tenancy::CourtOutcomeCodes::OUTRIGHT_POSSESSION_WITH_DATE => 'Outright possession with date',
+        Hackney::Tenancy::CourtOutcomeCodes::SUSPENSION_ON_TERMS => 'Suspension on terms',
+        Hackney::Tenancy::CourtOutcomeCodes::STRUCK_OUT => 'Struck out',
+        Hackney::Tenancy::CourtOutcomeCodes::WITHDRAWN_ON_THE_DAY => 'Withdrawn on the day',
+        Hackney::Tenancy::CourtOutcomeCodes::STAY_OF_EXECUTION => 'Stay of execution/Re-entry'
       }
 
       code_mapping[code]
